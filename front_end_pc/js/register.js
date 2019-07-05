068adcbc33f53ffda27b5c64b335fe5e0a87e0ec
python <857827469@qq.com>
2019-07-05 19:58:54 +0800

    即时判断手机号用户是否存在前端实现

--- front_end_pc/js/register.js
+++ front_end_pc/js/register.js
@@ -63,7 +63,23 @@ var vm = new Vue({
             } else {
                 this.error_name = false;
             }
-
+            // 检查重名
+            if (this.error_name == false) {
+                axios.get(this.host + '/usernames/' + this.username + '/count/', {
+                    responseType: 'json'
+                })
+                    .then(response => {
+                        if (response.data.count > 0) {
+                            this.error_name_message = '用户名已存在';
+                            this.error_name = true;
+                        } else {
+                            this.error_name = false;
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error.response.data);
+                    })
+            }
         },
         check_pwd: function (){
             var len = this.password.length;
@@ -87,6 +103,22 @@ var vm = new Vue({
             } else {
                 this.error_phone_message = '您输入的手机号格式不正确';
                 this.error_phone = true;
+            }
+            if (this.error_phone == false) {
+                axios.get(this.host + '/mobiles/' + this.mobile + '/count/', {
+                    responseType: 'json'
+                })
+                    .then(response => {
+                        if (response.data.count > 0) {
+                            this.error_phone_message = '手机号已存在';
+                            this.error_phone = true;
+                        } else {
+                            this.error_phone = false;
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error.response.data);
+                    })
             }
 
         },
