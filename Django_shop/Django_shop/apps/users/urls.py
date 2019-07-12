ebabc89c7d152e48fd428d615a86dccbac19eb98
python <857827469@qq.com>
2019-07-12 22:51:58 +0800

    历史记录前端实现

--- Django_shop/Django_shop/apps/users/urls.py
+++ Django_shop/Django_shop/apps/users/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^user/$', views.UserDetailView.as_view()),
     url(r'^email/$', views.EmailView.as_view()),  # 设置邮箱
     url(r'^emails/verification/$', views.VerifyEmailView.as_view()),
+    url(r'^browse_histories/$', views.UserBrowsingHistoryView.as_view()),
 ]
 
 router = routers.DefaultRouter()
